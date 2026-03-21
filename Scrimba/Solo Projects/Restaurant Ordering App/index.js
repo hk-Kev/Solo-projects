4bef4094743ee07ce6763ab9198df3b8feaabae7
HIRWA KAYIHURA Kevin <hirwakayihurakevin@gmail.com>
2026-03-21 18:37:00 +0200

    Finished Product

--- Scrimba/Solo Projects/Restaurant Ordering App/index.js
+++ Scrimba/Solo Projects/Restaurant Ordering App/index.js
@@ -1,22 +1,82 @@
 import { menuArray } from "/data.js";
 let menu = document.getElementById("menu-details");
+let orderedSection = document.getElementById("ordered-sec");
+let orderedItems = [];
+let totalOrdered = document.getElementById("total-ordered");
+let price = 0;
+let priceArr = [];
+let orderedHtml = ``;
+
+let thankYouMessage = `<p class="thanking">Thanks, Kevin! Your order is on it's way!</p>`;
+
+let itemDetails = "";
 
 let foodDetail = "";
+
+orderedHtml = `<h2>Your Order</h2>
+          <div id="ordered-sec"></div>
+
+          <div class="ordered total-ordered" id="total-ordered"></div>
+
+          <button class="pay-btn" id="complete-btn">Complete order</button>
+          `;
 menuArray.forEach((a) => {
   foodDetail += `
-    <div class="menu-sect" id="${a.id}">
-  <div class="left-side">
-  <p class="emoji-icon">${a.emoji}</p>
-  </div>
-  <div class="right-side">
-  <div class="food-details">
-  <h1>${a.name}</h1>
-      <p class="ingred">${a.ingredients}</p>
-      <p>$${a.price}</p>
-      </div>
-      <i class="fa-solid fa-circle-plus"></i>
-      </div>
-      </div>;
-      `;
+    <div class="menu-sect" >
+    <div class="left-side">
+    <p class="emoji-icon">${a.emoji}</p>
+    </div>
+    <div class="right-side">
+    <div class="food-details">
+    <h1>${a.name}</h1>
+    <p class="ingred">${a.ingredients}</p>
+    <p>$${a.price}</p>
+    </div>
+    <i class="fa-solid fa-circle-plus" id="${a.id}"></i>
+    </div>
+    </div>
+    `;
 });
 menu.innerHTML = foodDetail;
+
+menu.addEventListener("click", function (e) {
+  if (!isNaN(e.target.id)) {
+    orderedItems.push(menuArray[Number(e.target.id)]);
+    if (e.target.id == 1 || e.target.id == 0 || e.target.id == 2) {
+      document.getElementById("ordering-section").innerHTML = orderedHtml;
+    }
+    let totalOrdered = document.getElementById("total-ordered");
+    let orderedSection = document.getElementById("ordered-sec");
+
+    orderedItems.forEach((a) => {
+      itemDetails = `
+              <div class="ordered">
+              <h3>${a.name}</h3>
+              <p>${a.price}</p>
+              </div>
+              `;
+      price = a.price;
+      orderedSection.innerHTML += itemDetails;
+    });
+    priceArr.push(price);
+
+    totalOrdered.innerHTML = `
+                    <h3>Total price:</h3>
+                    <p>${priceArr.reduce((a, b) => a + b, 0)}</p>
+            `;
+
+    console.log(priceArr);
+  }
+
+  document
+    .getElementById("complete-btn")
+    .addEventListener("click", function () {
+      document.querySelector(".card-details").style.display = "block";
+    });
+});
+
+document.getElementById("pay-btn").addEventListener("click", function () {
+  document.querySelector(".card-details").style.display = "none";
+
+  document.getElementById("ordering-section").innerHTML = thankYouMessage;
+});
