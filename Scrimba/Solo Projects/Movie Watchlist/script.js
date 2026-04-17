58d8ca547a6247011c23eb35727532d5ccc842a8
HIRWA KAYIHURA Kevin <hirwakayihurakevin@gmail.com>
2026-04-17 14:57:48 +0200

    Movie Watchlist

---
+++ Scrimba/Solo Projects/Movie Watchlist/script.js
@@ -0,0 +1,7 @@
+// document.getElementById("btn").addEventListener("click", async () => {
+//   const res = await fetch(
+//     `http://www.omdbapi.com/?i=tt3896198&apikey=62de1c07`,
+//   );
+//   const data = await res.json();
+//   console.log(data);
+// });
