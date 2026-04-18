4feba008c5d96a43d2b0d2306e063c503dcacb67
HIRWA KAYIHURA Kevin <hirwakayihurakevin@gmail.com>
2026-04-18 19:46:56 +0200

    Slightly better version will comeback later

--- Scrimba/Solo Projects/Movie Watchlist/script.js
+++ Scrimba/Solo Projects/Movie Watchlist/script.js
@@ -61,28 +61,14 @@ mainSect.addEventListener("click", function (e) {
   localStorage.setItem("watchlist", JSON.stringify(watchlist));
 });
 
-for (let nana of watchlistArray) {
-  something = `<section class="movie">
-          <div class="poster-side" style="background-image:url(${nana.Poster})"></div>
-          <div class="details-side">
-            <div class="title-section">
-              <h3>${nana.Title} ${nana.Year}</h3>
-              <div>
-                <i class="fa-solid fa-star" id="star"></i> <span>${nana.imdbRating}</span>
-              </div>
-              </div>
-              <div class="movie-stats">
-                  <span class="runtime">${nana.Runtime}</span>
-                  <span class="genre">${nana.Genre}</span>
-                  <button class= "watch-btn" >
-                      <i class="fa-solid fa-circle-plus"></i> Watchlist
-                  </button>
-              </div>
-              <p class="plot-details">
-              ${nana.Plot}
-              </p>
-          </div>
-      </section>`;
-  document.getElementById("watchlist-section").innerHTML += something;
-  //   watchID++;
-}
+movieSearched.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchBtn.classList.add("btn-active-flash");
+
+    searchBtn.click();
+
+    setTimeout(() => {
+      searchBtn.classList.remove("btn-active-flash");
+    }, 150);
+  }
+});
--- Scrimba/Solo Projects/Movie Watchlist/watchlist.js
+++ Scrimba/Solo Projects/Movie Watchlist/watchlist.js
@@ -1,30 +1,60 @@
 let watchlistArray = JSON.parse(localStorage.getItem("watchlist")) || [];
 let something = "";
 let watchlistSection = document.getElementById("watchlist-section");
-watchlistSection.innerHTML = "";
+let listID = 0;
 
 for (let nana of watchlistArray) {
-  something = `<section class="movie">
-          <div class="poster-side" style="background-image:url(${nana.Poster})"></div>
-          <div class="details-side">
-            <div class="title-section">
-              <h3>${nana.Title} ${nana.Year}</h3>
-              <div>
-                <i class="fa-solid fa-star" id="star"></i> <span>${nana.imdbRating}</span>
-              </div>
-              </div>
-              <div class="movie-stats">
-                  <span class="runtime">${nana.Runtime}</span>
-                  <span class="genre">${nana.Genre}</span>
-                  <button class= "watch-btn" >
-                      <i class="fa-solid fa-circle-minus"></i> Remove
-                  </button>
-              </div>
-              <p class="plot-details">
-              ${nana.Plot}
-              </p>
-          </div>
-      </section>`;
+  something = `<section class="movie" id="${listID}">
+  <div class="poster-side" style="background-image:url(${nana.Poster})"></div>
+  <div class="details-side">
+  <div class="title-section">
+  <h3>${nana.Title} ${nana.Year}</h3>
+  <div>
+  <i class="fa-solid fa-star" id="star"></i> <span>${nana.imdbRating}</span>
+  </div>
+  </div>
+  <div class="movie-stats">
+  <span class="runtime">${nana.Runtime}</span>
+  <span class="genre">${nana.Genre}</span>
+  <button class= "watchlist-btn"" >
+  <i class="fa-solid fa-circle-minus"></i> Remove
+  </button>
+  </div>
+  <p class="plot-details">
+  ${nana.Plot}
+  </p>
+  </div>
+  </section>`;
   watchlistSection.innerHTML += something;
-  //   watchID++;
+  listID++;
+}
+
+document
+  .getElementById("watchlist-section")
+  .addEventListener("click", function (e) {
+    if (e.target.classList.contains("watchlist-btn")) {
+      const section = e.target.closest(".movie");
+      if (section) {
+        let id = section.id;
+
+        section.classList.add("vanisher");
+        console.log("it shouldve worked");
+        watchlistArray = watchlistArray.filter((item, index) => index != id);
+        localStorage.setItem("watchlist", JSON.stringify(watchlistArray));
+      }
+    }
+    checker();
+  });
+
+function checker() {
+  if (watchlistArray.length == 0) {
+    watchlistSection.innerHTML = `<section id="original-stuff">
+          <p>Your watchlist is looking a little empty....</p>
+
+          <p>
+            <i class="fa-solid fa-circle-plus"></i
+            ><a href="index.html"> Let's add some movies</a>
+          </p>
+        </section>`;
+  }
 }
